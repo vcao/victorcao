bceb62fce60afd34d077b8bf24095a3d0ae700d8
Victor Cao <victortcao@gmail.com>
2023-03-27 22:28:54 -0700

    fix builder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: "static",
   css: ["@/assets/css/styles.css"],
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
   postcss: {
