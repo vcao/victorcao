14c5b2d5ad4094aef11af44e8673ecedc3e912ef
Victor Cao <victortcao@gmail.com>
2023-03-30 13:46:36 -0700

    fix deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   css: ["@/assets/css/styles.css"],
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
   postcss: {
