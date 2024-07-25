3d3115bdafbfcd551143176d44aca3823c7c0ea4
Victor Cao <victortcao@gmail.com>
2024-07-25 14:35:09 -0700

    updated

--- app.vue
+++ app.vue
@@ -14,29 +14,11 @@
           <h1
             class="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl"
           >
-            Victor Cao <span class="italic font-serif text-3xl align-vertical font-semibold text-gray-600">for</span> Woodbury East HOA Board of Directors
+            Victor Cao
           </h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">
-            As a father and homeowner, my vision for Woodbury East is to:
-            <ol class="list-decimal">
-              <li>Ensure our community is welcoming and inclusive of all its residents</li>
-              <li>Improve fiscal management, keep dues assessments reasonable without any sudden spikes</li>
-              <li>Create memorable experiences with events & amenities</li>
-            </ol>
+            Just another landing page
           </p>
-          <p class="mt-6 text-lg leading-8 text-gray-600">
-            As a board member, I will actively solicit community input so that we can create a shared vision for Woodbury East.
-          </p>
-          <div class="mt-10 flex items-center gap-x-6">
-            <a
-              href="#"
-              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >Vote for Victor Cao by June 9</a
-            >
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
-              >Learn more <span aria-hidden="true">→</span></a
-            >
-          </div>
         </div>
       </div>
       <div
@@ -44,13 +26,11 @@
       >
         <img
           class="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-          src="@/assets/img/hero-vkfamily.jpg"
+          src="https://images.pexels.com/photos/1784577/pexels-photo-1784577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt=""
         />
       </div>
     </div>
   </div>
   <!-- End Hero -->
-  
-
 </template>
